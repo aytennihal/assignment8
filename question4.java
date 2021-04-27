57eb68122199bb0c55691e7dc9eb333c3d93dfb1
aytennihal <80786330+aytennihal@users.noreply.github.com>
2021-04-27 15:42:35 -0500

    Add files via upload

---
+++ question4.java
@@ -0,0 +1,19 @@
+package assignment8;
+
+public class question4 {
+
+	public static void main(String[] args) {
+		
+		printHollowRect();
+	}
+	
+	public static void printHollowRect() {
+		
+		
+		System.out.println("*****");
+		System.out.println("*   *");
+		System.out.println("*   *");
+		System.out.println("*   *");
+		System.out.println("*****");
+	}
+}
